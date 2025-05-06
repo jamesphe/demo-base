7de640bcdc7fd6250c950791c042cc29cc405cd7
james_phe <james_phe@126.com>
2025-05-06 09:20:12 +0800

    代码优化

--- backend/scripts/init_db.sh
+++ backend/scripts/init_db.sh
@@ -12,5 +12,6 @@ echo "正在初始化数据库..."
 
 # 执行SQL脚本
 PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_SERVER -p $POSTGRES_PORT -U $POSTGRES_USER -f init.sql
+PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_SERVER -p $POSTGRES_PORT -U $POSTGRES_USER -f init_permissions.sql
 
 echo "数据库初始化完成!" 
